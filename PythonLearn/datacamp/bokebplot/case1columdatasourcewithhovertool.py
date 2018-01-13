f287bdedad70fe7569e7f7d11c469cc5ecf858cc
James <James@James>
2018-01-12 20:43:24 -0500

    add comment to case1 in bokplot

--- PythonLearn/datacamp/bokebplot/case1columdatasourcewithhovertool.py
+++ PythonLearn/datacamp/bokebplot/case1columdatasourcewithhovertool.py
@@ -4,6 +4,7 @@ from bokeh.plotting import figure
 from bokeh.models import HoverTool,ColumnDataSource
 
 # Make the ColumnDataSource: source
+# add comment
 source = ColumnDataSource(data={
     'x'       : data.loc[1970].fertility,  #this way conver to seriesr
     'y'       : data.loc[1970].life,
